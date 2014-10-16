1410382d672db3ee6bbd095441555b2c50f22eb0
Zag2me <doowles2@hotmail.com>
2014-10-16 11:16:41 +0100

    [Feature] Add icons for ICT support Technicians and stats to front page

--- index.php
+++ index.php
@@ -25,14 +25,29 @@ include "includes/db_connection.php";
 				// Create an array of all the tickets
 				$total_jobs_all = $db->get_var("SELECT Count(*) FROM tickets");
 				$total_jobs_open = $db->get_var("SELECT Count(*) FROM tickets WHERE intOpenJob = 1");
+				$admin_users = $db->get_results("SELECT * FROM users WHERE strAdmin = 'ON'");
 				
 				// Draw the website logo and start the table
-				echo "<img src='images/icons/give_computer.png'><br><p><table width='400' border='0'><tr>";
+				echo "<img src='images/icons/give_computer.png'>";
+				echo "<br><p>";
 				
-				// Write the total and open jobs to screen  
-				echo "<tr><td><center><h2><img src='images/icons/spanner.png'><br><b><a href='show_jobs.php?show_open=0'>Total Jobs:</a>  </b>" . $total_jobs_all . "</h2></center><br></td>";
-				echo "<td><center><h2><img src='images/icons/job_open.png'><br><b><a href='show_jobs.php?show_open=1'>Open Jobs:</a> </b>" . $total_jobs_open . "</h2></center><br></td></tr>";
+				// Write the total and open jobs to screen
+				echo "<table width='400' border='0'><tr>";
+				echo "<tr><td><center><h2><img src='images/icons/spanner.png'><br><b><a href='show_jobs.php?show_open=0'>Total Jobs:</a>  </b>" . number_format($total_jobs_all) . "</h2></center><br></td>";
+				echo "<td><center><h2><img src='images/icons/job_open.png'><br><b><a href='show_jobs.php?show_open=1'>Open Jobs:</a> </b>" . number_format($total_jobs_open) . "</h2></center><br></td></tr>";
 				echo "</table>";
+
+				// Write the total jobs for each ICT technician
+				echo "<table width='400' border='0'><tr>";
+				if ($admin_users != NULL) 
+						{
+							foreach ($admin_users as $admin_user)
+							{
+								$total_jobs_admin = $db->get_var("SELECT Count(*) FROM tickets WHERE strUserComplete = '" . $admin_user->strEmail . "'");
+								echo "<td><center><h2><img src='images/avatars/" . $admin_user->strName . ".jpg'><br><b><a href='show_jobs.php?show_open=0'></a>  </b>" . $admin_user->strName . ": " . number_format($total_jobs_admin) . "</h2></center><br></td>";
+							}
+						}
+				echo "</tr></table>";
 				?>
 				
 				</p>
